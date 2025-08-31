ca02b716c83c5bd14f9020c05b499e500b035aba
kamlesh <kamlesh81473@gmail.com>
2025-08-31 12:24:24 +0530

    add new code

---
+++ models/listing.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema= new Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    description:String,
+    image:{
+        type:String,
+        default:"https://unsplash.com/photos/a-butterfly-rests-on-a-pink-flower-OTAojt0y3-c",
+        set:(v) =>
+            v === ""
+        ? "https://unsplash.com/photos/a-butterfly-rests-on-a-pink-flower-OTAojt0y3-c":v,
+    },
+    price:Number,
+    location:String,
+    country:String,
+
+});
+const Listings= mongoose.model("Listing",listingSchema);
+module.exports=Listings;
