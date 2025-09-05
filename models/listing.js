90d9b6f0f938b8fda1ec2ed5a516fcea6c8fbb3e
kamlesh <kamlesh81473@gmail.com>
2025-09-05 09:15:36 +0530

    add new code

--- models/listing.js
+++ models/listing.js
@@ -7,12 +7,12 @@ const listingSchema= new Schema({
         required:true,
     },
     description:String,
-//    image: {
-//     type: String,
-// //     default: "https://unsplash.com/photos/a-butterfly-rests-on-a-pink-flower-OTAojt0y3-c",
-//     set: (v) =>
-//         v === "" ? "https://unsplash.com/photos/a-butterfly-rests-on-a-pink-flower-OTAojt0y3-c" : v,
-// },
+   image: {
+    type: String,
+    default: "https://unsplash.com/photos/palm-trees-silhouetted-against-a-golden-ocean-sunset-l4i5j3NU1M0",
+    set: (v) =>
+        v === "" ? "https://unsplash.com/photos/silhouette-of-person-holding-staff-against-sunset-over-ocean-sXe-WIguxA0" : v,
+},
     price:Number,
     location:String,
     country:String,
