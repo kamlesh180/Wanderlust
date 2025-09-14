4d40d57c677a50720f143f9e1a6c0a25fbf1b2c4
kamlesh <kamlesh81473@gmail.com>
2025-09-14 15:23:16 +0530

    add new code

--- app.js
+++ app.js
@@ -72,8 +72,8 @@ app.get("/listings/new",(req,res) =>{
 //show route
 app.get ("/listings/:id" ,wrapAsync(async (req,res) => {
     let {id}=req.params;
-    const listing = await Listing.findById(id).populate("reviews");
-    res.render("listings/show.ejs", { listing });
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing});
 })
 );
 
