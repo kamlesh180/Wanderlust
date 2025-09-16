7acab95298213504f6c87c613cbce6c301216da7
kamlesh <kamlesh81473@gmail.com>
2025-09-16 21:22:24 +0530

    add new code

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema , reviewSchema} = require("./schema.js");
-const Review = require("./models/review.js");
 
 const listing = require ("./routes/listing.js");
+const reviews = require("./routes/review.js");
  
 const MONGO_URL ="mongodb://127.0.0.1:27017/wanderlust";
 
@@ -37,42 +34,10 @@ app.get("/",(req,res) =>{
     res.send("Hi, I am root");
 });
 
-const validateReview = (req,res,next) => {
-   let {error} = reviewSchema.validate(req.body);
-if (error){
-    let errMsg = error.details.map((el) =>
-    el.message).join (",");
-throw new ExpressError(400,errMsg)
-} else {
-    next();
-}
-};
 
 app.use("/listings", listing);
+app.use("/listings/:id/reviews",reviews);
 
-//Reviews
-//Post review route
-app.post("/listings/:id/reviews",validateReview, wrapAsync(async(req,res) => {
-   let listing= await Listing.findById(req.params.id);
-   let newReview = new Review(req.body.review);
-
-   listing.reviews.push(newReview._id);
-
-   await newReview.save();
-   await listing.save();
-   res.redirect(`/listings/${listing._id}`);
-// res.send("Review added successfullt");
-}));
-
-//Delete review route
-app.delete("/listings/:id/reviews/:reviewId",
-     wrapAsync(async(req,res) => {
-    let {id,reviewId} = req.params;
-    await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId   }});
-    await Review.findByIdAndDelete (reviewId);
-    res.redirect(`/listings/${id}`);
-})
-);
 
 //Middleware error handling
 app.all('/*splat', (req, res, next) => {
