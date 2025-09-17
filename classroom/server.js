bb103f338b0da8c66eabfbed369feb0441af7c7c
kamlesh <kamlesh81473@gmail.com>
2025-09-17 20:12:13 +0530

    add new code

--- classroom/server.js
+++ classroom/server.js
@@ -4,14 +4,24 @@ const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
 const session = require("express-session");
 
-app.use (session({secret:"mysupersecretstring"}));
+app.use (session({secret:"mysupersecretstring",
+    resave:false,
+    saveUninitialized:true}));
 
-app.get("/test",(req,res) => {
-    res.send("test successful!");
+app.get("/reqcount",(req,res) => {
+    if (req.session.count) {
+        req.session.count++
+    } else {
+        req.session.count = 1;
+    }
+    res.send(`You have visited this page ${req.session.count} times`);
 });
-app.use("/users", users);
-app.use("/posts", posts);
+// app.get("/test",(req,res) => {
+//     res.send("test successful!");
+// });
+// app.use("/users", users);
+// app.use("/posts", posts);
 
 app.listen (3000, () => {
-    console.log("Server is running on port 3000");
+    console.log("Server is listeing to  3000");
 });
