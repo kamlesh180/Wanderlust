1243f5c49f510037afcc703f896160a9cee420d3
kamlesh <kamlesh81473@gmail.com>
2025-09-17 18:14:08 +0530

    add new code

--- classroom/server.js
+++ classroom/server.js
@@ -2,32 +2,13 @@ const express = require("express");
 const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
-const cookieParser = require("cookie-parser");
-app.use(cookieParser("secretcode"));
-app.get("/getsignedcookies", (req,res) => {
-    res.cookie("made-in","India",{signed:true});
-    res.send("signed cookie sent");
-});
-app.get("/verify",(req,res) => {
-    console.log(req.signedCookies);
-    res.send(req.signedCookies);
-});
+const session = require("express-session");
 
-app.get("/getcookies", (req,res) => {
-    res.cookie("greet", "namaste");
-    res.cookie("origin","India");
-    res.send("sent you some cookies");
-});
-app.get("/greet",(req,res) => {
-    let {name = "anonymous"} =req.cookies;
-    res.send(`Hi,${name}`);
-}) 
+app.use (session({secret:"mysupersecretstring"}));
 
-app.get("/", (req,res) => {
-    console.dir(req.cookies);
-    res.send("Hi,I am root");
+app.get("/test",(req,res) => {
+    res.send("test successful!");
 });
-
 app.use("/users", users);
 app.use("/posts", posts);
 
