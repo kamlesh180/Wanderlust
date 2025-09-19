370fc016ea6385c3e6263a75ebb0a85f17c1a977
kamlesh <kamlesh81473@gmail.com>
2025-09-19 13:51:11 +0530

    add cookies option

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const ExpressError = require("./utils/ExpressError.js");
 const listing = require ("./routes/listing.js");
 const reviews = require("./routes/review.js");
 const Listings = require("./models/listing.js");
+const session = require("express-session");
  
 const MONGO_URL ="mongodb://127.0.0.1:27017/wanderlust";
 
@@ -30,6 +31,19 @@ app.use(express.urlencoded({extended:true}));
 app.use (methodOverride ("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
+
+const sessionOptions = {
+    secret:"mysupersecretcode",
+    resave:false,
+    saveUninitialized:true,
+    cookie:{
+        expires: Date.now() + 7*24*60*60*1000,
+        maxAge: 7*24*60*60*1000,
+
+    },
+
+};
+app.use(session(sessionOptions));
 
 app.get("/",(req,res) =>{
     res.send("Hi, I am root");
