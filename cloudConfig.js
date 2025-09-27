838629945d4137e38565c5af7003d189ae218fe9
kamlesh <kamlesh81473@gmail.com>
2025-09-27 21:25:49 +0530

    setup colude

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,11 +1,13 @@
 const cloudinary=require("cloudinary").v2;
-const { cloudinaryStorage } = require("multer-storage-cloudinary");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+
+
 cloudinary.config({
     cloud_name: process.env.CLOUDE_NAME,
     api_key: process.env.CLOUDE_API_KEY,
     api_secret:process.env.CLOUDE_API_SECRET
 }); 
-const storage= new cloudinaryStorage({
+const storage= new CloudinaryStorage({
     cloudinary:cloudinary,
     params:{
         folder:"wanderlust-DEV",
