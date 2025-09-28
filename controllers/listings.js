293cad52e5f09d53739365dd40dd78c6400f2fc7
kamlesh <kamlesh81473@gmail.com>
2025-09-28 16:35:16 +0530

    create image pre for edit page

--- controllers/listings.js
+++ controllers/listings.js
@@ -44,7 +44,10 @@ module.exports.renderEditForm = async (req,res) => {
         req.flash("error","Listing you are requested does not exist");
     return res.redirect("/listings");
     }
-    res.render("listings/edit.ejs" ,{listing});
+    
+    let originalImageUrl = listing.image.url;
+   originalImageUrl= originalImageUrl.replace("/upload","/upload/w_250");
+    res.render("listings/edit.ejs",{listing , originalImageUrl});
 };
 
 module.exports.updateListing = async (req,res) => {
