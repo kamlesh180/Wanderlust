a285179c116e539845fe5b0299255a2dfe077b05
kamlesh <kamlesh81473@gmail.com>
2025-09-28 14:44:23 +0530

    save image link in mongo

--- routes/listing.js
+++ routes/listing.js
@@ -13,15 +13,14 @@ const upload = multer({ storage});
 router
 .route("/")
 .get(wrapAsync(listingController.index))
-//  .post(
-//     isLoggedIn,
-//     validateListing,
-//     wrapAsync(listingController.createListing)
-//     );
+ .post(
+    isLoggedIn,
+    // validateListing,
+    upload.single("listing[image])"),
+    wrapAsync(listingController.createListing)
+    );
 
-.post(upload.single("listing[image]"),(req,res) => {
-    res.send(req.file);
-});
+
 
 //new route 
 router.get("/new",isLoggedIn,listingController.renderNewForm);
