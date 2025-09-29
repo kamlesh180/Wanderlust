a46f28a640b772f6b0b3e12c78640588c4d6c980
kamlesh <kamlesh81473@gmail.com>
2025-09-29 22:43:28 +0530

    add new code

--- controllers/controll .js
+++
@@ -1,9 +0,0 @@
-const Listing = require("../models/listing");
-module.exports.index=async (req,res) => {
-const allListings = await Listing.find({});
-res.render("./listings/index.ejs", {allListings});
-};
-
-module.exports.renderNewForm = (req,res) => {
-res.render("listings/new.ejs");
-}
